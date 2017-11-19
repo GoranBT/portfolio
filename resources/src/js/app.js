59ccbf6249d32f428a3c382bdd7bfa081ba9f057
GoranBT <goran.angelovski.bt@gmail.com>
2017-11-19 23:37:11 +0000

    add flexbox structure

--- resources/src/js/app.js
+++ resources/src/js/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+$(document).ready(function(){
+  console.log('hello');
+
+  $('.test').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
+
+
+$(document).ready(function(){
+  $('.hamburger').click(function(){
+    $(this).toggleClass('is-active');
+  });
+});
