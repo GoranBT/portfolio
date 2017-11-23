e61ec96e965d548611f3043f86e65b5f6c22b5a7
GoranBT <goran.angelovski.bt@gmail.com>
2017-11-23 23:54:52 +0000

    add photos, header and footer

--- resources/src/js/app.js
+++ resources/src/js/app.js
@@ -1,18 +1,48 @@
 $(document).ready(function(){
   console.log('hello');
 
-  $('.test').slick({
+  $('.carousel').slick({
     dots: true,
     infinite: true,
     speed: 500,
     fade: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+    prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>'
   });
-});
 
 
-$(document).ready(function(){
-  $('.hamburger').click(function(){
-    $(this).toggleClass('is-active');
+  $('.menu').on('click', function(){
+    $('.mobile').toggleClass('active');
+    // $('.mobile').toggleClass('show_list');
+    // $('.mobile').fadeIn();
   });
+
+
+  $('.hamburger').on('click', function(){
+    $('.hamburger').toggleClass('is-active');
+  });
+
+
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 150) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop: 0},1000);
+    return false;
+
+  });
+
+  $('.testing').slideDown();
+
+
+
+
 });
